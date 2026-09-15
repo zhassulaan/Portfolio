c7e295587257defed5fb0ac3b52fd0cf809c27e9
zhassulaan <74059375+zhassulaan@users.noreply.github.com>
2026-09-15 11:29:41 +0000

    Deploying to gh-pages from @ zhassulaan/Portfolio@93a6b203432a15d8f07bb42530140820e2303011 🚀

---
+++ _nuxt/B4rZBr6N.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./1Z-JJ1Zx.js";import{e as s,Q as t,K as n,i as a,g as r,o as u}from"./DFDKhhyk.js";import"./D5WGEPrD.js";const d=s({__name:"[slug]",setup(c){const o=t().params.slug;return n(()=>{window.scrollTo({top:0,left:0,behavior:"instant"})}),(i,m)=>(u(),a(e,{kind:"recommendation",slug:r(o)},null,8,["slug"]))}});export{d as default};
