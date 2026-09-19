385b09a068a48003e4089ffde22ca7deac9557c9
zhassulaan <74059375+zhassulaan@users.noreply.github.com>
2026-09-19 19:19:23 +0000

    Deploying to gh-pages from @ zhassulaan/Portfolio@97fffbc69153ad20d346d4199020b1944bd1e81d 🚀

--- _nuxt/BB45Odmk.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./CoARmjll.js";import{e as s,Q as t,K as n,i as a,g as r,o as u}from"./G4ib9BhL.js";import"./C9MooEu2.js";const g=s({__name:"[slug]",setup(c){const o=t().params.slug;return n(()=>{window.scrollTo({top:0,left:0,behavior:"instant"})}),(l,p)=>(u(),a(e,{kind:"certificate",slug:r(o)},null,8,["slug"]))}});export{g as default};
