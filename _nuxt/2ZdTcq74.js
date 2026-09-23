2a28f229f2f9c26431bd44bf043c1c87ab871ea1
zhassulaan <74059375+zhassulaan@users.noreply.github.com>
2026-09-23 19:57:18 +0000

    Deploying to gh-pages from @ zhassulaan/Portfolio@5d87d6c6ce2369ca541b263fbb15fb00ffc42811 🚀

---
+++ _nuxt/2ZdTcq74.js
@@ -0,0 +1,1 @@
+import{S as r}from"./eAsy6mzC.js";function u(){const t=r("locale_dictionary",()=>({}));function e(o,n){return t.value[o]??n}return{tx:e}}export{u};
---
+++ _nuxt/DCiXx2f3.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./Ds8Lxs5A.js";import{d as e,Q as t,K as n,h as a,u as r,o as u}from"./eAsy6mzC.js";import"./2ZdTcq74.js";const d=e({__name:"[slug]",setup(c){const o=t().params.slug;return n(()=>{window.scrollTo({top:0,left:0,behavior:"instant"})}),(m,i)=>(u(),a(s,{kind:"recommendation",slug:r(o)},null,8,["slug"]))}});export{d as default};
